27f41588d46a4a0f3ceba3c030d6ce31b52ca4c9
Matthew Cabral <matthew.larbac@gmail.com>
2021-12-16 13:52:42 -0500

    renaming

--- src/components/MyWebsite.js
+++ src/components/MyWebsite.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 // Components
 import Nav from './nav/Nav';
-import Welcome from './Welcome/Welcome';
-import Projects from './projects/Projects';
-import Work from './work/Work';
-import Contact from './contact/Contact';
+import Welcome from './Welcome/Welcome.js';
+import Projects from './projects/Projects.js';
+import Work from './work/Work.js';
+import Contact from './contact/Contact.js';
 
 const MyWebsite = () => {
     return (
